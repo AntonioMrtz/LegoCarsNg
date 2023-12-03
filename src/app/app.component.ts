46cee4e572c920e8f412e286fcd2049abf3b306e
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-12-03 13:46:28 +0100

    feat : add footer header main-content structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Sidebar } from './sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderProfile } from './header-profile/header-profile.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,Sidebar,RouterOutlet],
+  imports: [CommonModule,Sidebar,RouterOutlet,HeaderProfile,AppFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/header-profile/header-profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'header-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header-profile.component.html',
+  styleUrl: './header-profile.component.scss'
+})
+export class HeaderProfile {
+
+}
