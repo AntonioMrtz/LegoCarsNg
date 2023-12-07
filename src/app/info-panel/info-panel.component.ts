725335813a15a92949eebb90c4d7db6a10a0fc5f
Raúl Zamora <raulzamoracanovas@gmail.com>
2023-12-07 19:50:02 +0100

    feat: Home component with defer example + infopanel component
    Creado el home component con varias imágenes y una ultima más grande para ejemplificar el uso de defer. Para esto ultimo se ha creado un componente extra info-panel.

---
+++ src/app/info-panel/info-panel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-info-panel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './info-panel.component.html',
+  styleUrl: './info-panel.component.scss'
+})
+export class InfoPanelComponent {
+
+}
